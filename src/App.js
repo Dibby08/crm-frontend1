1d7b8ac40d30eadd1e436573dcd9e40246fff0c0
Dipesh Simkhada <dipeshsimkhada@192-168-1-112.tpgi.com.au>
2020-11-02 18:04:03 +1100

    App set up. added login and reset password form

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Entry } from './pages/entry/entry.page';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Entry />
+    </div>
+  );
+}
+
+export default App;
